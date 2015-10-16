fe09a0ffc52eb370b34bc31658649c52a8f59e75
shannonshsu <shannonshsu@gmail.com>
2015-10-16 15:02:17 -0700

    added json elements

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,78 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var education = {
+	"schools": [
+	{
+		"name": "Williams College",
+		"location": "Williamstown",
+		"majors": "Mathematics",
+		"dates": "June 2015",
+		"url": "url here",
+		"courses": "courses here"
+	},
+	{
+		"name": "Arcadia High School",
+		"location": "Arcadia",
+		"dates": "June 2011",
+		"url": "ausd.net"
+	}
+	],
+	"onlineCourses": [
+	{
+		"title": "course title",
+		"school": "udacity",
+		"dates": "dates here",
+		"url": "url here"
+	}
+	]
+};
+
+var work = {
+	"jobs": [
+	{
+		"employer": "Employer Here",
+		"title": "City Here",
+		"location": "Location Here",
+		"dates": "Dates Here",
+		"description": "Description Here line 1 \
+		line 2 \
+		line 3"
+	},
+	{
+		"employer": "Employer Here",
+		"title": "City Here",
+		"location": "Location Here",
+		"dates": "Dates Here",
+		"description": "Description Here"
+	}
+	]
+};
+
+var projects = {
+	"project": [
+	{
+		"title": "Project Title",
+		"dates": "Date Here",
+		"description": "Description Here",
+		"images": ["URL1", "URL2", "URL3"]
+	},
+	{
+		"title": "Project Title 2",
+		"dates": "Date Here 2",
+		"description": "Description Here 2",
+		"images": ["URL1", "URL2", "URL3"]
+	}
+	]
+};
+
+var bio = {
+	"name": "Shannon Hsu",
+	"role": "Role Here",
+	"welcomeMessage": "Welcome Message here",
+	"contacts": {
+		"mobile": "6269757618",
+		"email": "shannonshsu@gmail.com",
+		"github": "github.com/shannonshsu",
+		"location": "Arcadia"
+	},
+	"skills": ["skill 1", "skill2", "skill3"]
+};
+
